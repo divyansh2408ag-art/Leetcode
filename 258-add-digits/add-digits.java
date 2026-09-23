f76c8ada514a73b24f38c22c6a710232d5f48322
divyansh2408ag-art <divyansh2408.ag@gmail.com>
2026-09-23 19:45:38 +0530

    Time: 1 ms (98.09%) | Memory: 42.5 MB (79.70%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,15 +1,10 @@
 class Solution {
     public int addDigits(int num) {
-        int sum;
-        while(num>=10)
-        {
-            sum=0;
-            while(num>0){
-            sum+=num%10;
-            num/=10;
-            }
-            num=sum;
+        if(num == 0){
+            return 0;
+        } else if (num % 9 ==0){
+            return 9;
         }
-        return num;
+        return num % 9;
     }
 }
